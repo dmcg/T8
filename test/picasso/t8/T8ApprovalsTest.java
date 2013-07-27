6a78f32d6120582ae0fbaf3905be9a3734149fba
Duncan McGregor <duncan@oneeyedmen.com>
2013-07-27 09:41:16 -0700

    Tidy approvals and commit them

--- test/picasso/t8/T8ApprovalsTest.java
+++ test/picasso/t8/T8ApprovalsTest.java
@@ -1,12 +1,7 @@
 package picasso.t8;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
+import com.google.common.base.Function;
 import com.google.common.io.Files;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.rococoa.okeydoke.Transcript;
@@ -15,52 +10,68 @@ import org.rococoa.okeydoke.junit.ApprovalsRule;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class T8ApprovalsTest {
 
     @Rule public final ApprovalsRule approver = ApprovalsRule.fileSystemRule("test");
-    private Transcript transcript;
-
-    @Before public void setup() throws IOException {
-        transcript = approver.transcript();
-    }
+    private final File file = new File("wordlist.txt");
+    private final T8 t8 = new T8(readWords(file));
 
     @Test public void suggestions() throws IOException {
-        File file = new File("wordlist.txt");
-        T8 t8 = new T8(readWords(file));
-
         given("the words in ", file);
-        when("I enter", "2");
-        then("the search is made for ", t8.suggestionsFor("2"));
+        whenThen("I enter", "duck", "then the suggestions are", new Function<String, Object>() {
+            public Object apply(String input) {
+                return t8.suggestionsFor(input);
+            }
+        });
     }
 
-
-    private Transcript given(String given, Object thing) throws IOException {
-        return term("given", given, thing);
+    @Test public void roots() throws IOException {
+        given("the words in ", file);
+        whenThen("I enter", "duck", "then the suggestions are", new Function<String, Object>() {
+            public Object apply(String input) {
+                return t8.rootsFor(input);
+            }
+        });
     }
 
-    private Transcript when(String when, Object thing) throws IOException {
-        return term("when", when, thing);
+    private void whenThen(String when, String word, String then, Function<String, Object> f) throws IOException {
+        when(when, word);
+        String input = t8.inputFor(word);
+        then(then, f.apply(input));
     }
 
-    private Transcript then(String when, Object thing) throws IOException {
-        return term("then", when, thing);
+    private void given(String given, Object thing) throws IOException {
+        term("given", given, thing);
     }
 
-    private Transcript term(String term, String description, Object thing) {
-        transcript.append(term + " " + description + " ").appendFormatted(thing).endl();
-        return transcript;
+    private void when(String when, Object thing) throws IOException {
+        term("when", when, thing);
     }
 
-    public List<String> readWords(File file) throws IOException {
-        return Files.readLines(file, Charset.defaultCharset());
+    private void then(String then, Object thing) throws IOException {
+        term("then", then, thing);
+    }
+
+    private void term(String term, String description, Object thing) throws IOException {
+        transcript().append(term + " " + description + " ").appendFormatted(thing).endl();
+    }
+
+    private Transcript transcript() throws IOException {
+        return approver.transcript();
+    }
+
+    public List<String> readWords(File file) {
+        try {
+            return Files.readLines(file, Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
