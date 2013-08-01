98d903fb4e69518a770a42fd30d1c7290b311116
Duncan McGregor <duncan@oneeyedmen.com>
2013-08-01 09:59:10 -0700

    Start using okeydoke TableFormatter

--- test/picasso/t8/T8ApprovalsTest.java
+++ test/picasso/t8/T8ApprovalsTest.java
@@ -1,17 +1,15 @@
 package picasso.t8;
 
 import com.google.common.base.Function;
-import com.google.common.io.Files;
 import org.junit.Rule;
 import org.junit.Test;
-import org.rococoa.okeydoke.Transcript;
-import org.rococoa.okeydoke.bdd.Pickle;
-import org.rococoa.okeydoke.bdd.Scenario;
+import org.rococoa.okeydoke.formatters.TableFormatter;
 import org.rococoa.okeydoke.junit.ApprovalsRule;
+import org.rococoa.okeydoke.pickle.Pickle;
+import org.rococoa.okeydoke.pickle.Scenario;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -22,13 +20,13 @@ public class T8ApprovalsTest {
 
     @Rule public final ApprovalsRule approver = ApprovalsRule.fileSystemRule("test");
     private final File file = new File("wordlist.txt");
-    private final T8 t8 = new T8(readWords(file));
+    private final T8 t8 = new T8(FileReader.readWords(file));
 
     @Test public void suggestions() throws IOException {
         Scenario scenario = new Pickle(approver.transcript()).scenario("Suggestions");
         scenario.given("the words in", file);
-        whenThen(scenario, "I enter", "duck", "the suggestions are", new Function<String, Object>() {
-            public Object apply(String input) {
+        whenThen(scenario, "I enter", "duck", "the suggestions are", new Function<String, Iterable<?>>() {
+            public Iterable<String> apply(String input) {
                 return t8.suggestionsFor(input);
             }
         });
@@ -37,25 +35,18 @@ public class T8ApprovalsTest {
     @Test public void roots() throws IOException {
         Scenario scenario = new Pickle(approver.transcript()).scenario("Roots");
         scenario.given("the words in", file);
-        whenThen(scenario, "I enter", "duck", "the roots are", new Function<String, Object>() {
-            public Object apply(String input) {
+        whenThen(scenario, "I enter", "duck", "the roots are", new Function<String, Iterable<?>>() {
+            public Iterable<?> apply(String input) {
                 return t8.rootsFor(input);
             }
         });
     }
 
-    private void whenThen(Scenario scenario, String when, String word, String then, Function<String, Object> f) throws IOException {
+    private void whenThen(Scenario scenario, String when, String word, String then, Function<String, Iterable<?>> f) throws IOException {
         scenario.when(when, word);
         String input = t8.inputFor(word);
-        scenario.then(then, f.apply(input));
-    }
-
-    public List<String> readWords(File file) {
-        try {
-            return Files.readLines(file, Charset.defaultCharset());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        scenario.then(then);
+        scenario.appendFormatted(f.apply(input), TableFormatter.instance());
     }
 
 }
