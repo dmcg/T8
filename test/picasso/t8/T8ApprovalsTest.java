3f77d01b440d383e1b1752e596c9888f7b8dc978
Duncan McGregor <duncan@oneeyedmen.com>
2013-08-10 11:44:58 +0100

    Use new fluent then

--- test/picasso/t8/T8ApprovalsTest.java
+++ test/picasso/t8/T8ApprovalsTest.java
@@ -4,10 +4,15 @@ import com.google.common.base.Function;
 import org.jmock.api.Invocation;
 import org.jmock.api.Invokable;
 import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.rococoa.okeydoke.Formatter;
+import org.rococoa.okeydoke.formatters.DefaultInvocationFormatter;
 import org.rococoa.okeydoke.formatters.TableFormatter;
+import org.rococoa.okeydoke.internal.Fred;
+import org.rococoa.okeydoke.internal.Mapper;
 import org.rococoa.okeydoke.internal.MappingIterable;
 import org.rococoa.okeydoke.pickle.Feature;
 import org.rococoa.okeydoke.pickle.FeatureRule;
@@ -15,6 +20,8 @@ import org.rococoa.okeydoke.pickle.ScenarioRule;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import static org.rococoa.okeydoke.junit.ApprovalsRule.fileSystemRule;
@@ -25,38 +32,47 @@ public class T8ApprovalsTest {
     @ClassRule public static final FeatureRule feature = new FeatureRule(fileSystemRule("test"));
     @Rule public final ScenarioRule scenario = feature.scenarioRule();
 
-    private final File file = new File("wordlist.txt");
-    private final T8 t8 = new T8(FileReader.readWords(file));
+    private T8 t8;
+
+    @Before public void given() {
+        File file = new File("wordlist.txt");
+        scenario.given("the words in", file);
+        t8 = new T8(FileReader.readWords(file));
+    }
 
     @Test public void suggestions() throws IOException {
-        scenario.given("the words in", file);
-        whenThen("I enter", "duck", "the suggestions are", new Function<String, Iterable<?>>() {
-            public Iterable<String> apply(String input) {
-                return t8.suggestionsFor(input);
-            }
-        });
+        String input = t8.inputFor("duck");
+        scenario.given("the input for", "duck", "is", input);
+        scenario.when("I enter", input);
+        thenWith(t8, "the suggestions are\n", TableFormatter.instance()).suggestionsFor(input);
     }
 
     @Test public void roots() throws IOException {
-        scenario.given("the words in", file);
-        whenThen("I enter", "duck", "the roots are", new Function<String, Iterable<?>>() {
-            public Iterable<?> apply(String input) {
-                return columnise(t8.rootsFor(input));
-            }
-        });
+        String input = t8.inputFor("duck");
+        scenario.given("the input for", "duck", "is", input);
+        scenario.when("I enter", input);
+        thenWith(t8, "the suggestions are\n",
+                new TableFormatter().withMapper(rootMapper()).withHeaders("root", "freq")).rootsFor(input);
     }
 
-    private Iterable<?> columnise(final List<Root> roots) {
-        return new MappingIterable<Object, Root>(roots) {
-            @Override protected Object map(Root thing) {
-                return new Object[] {thing.root, thing.frequency};
-            }
+    private Mapper<Root, Object[]> rootMapper() {
+        return new Mapper<Root, Object[]>() {
+            @Override
+            public Object[] map(Root next) {
+                return new Object[] {next.root, next.frequency};            }
         };
     }
 
-    private void whenThen(String when, String word, String then, Function<String, Iterable<?>> f) throws IOException {
-        scenario.when(when, word);
-        String input = t8.inputFor(word);
-        scenario.then(then + "\n", TableFormatter.instance(), f.apply(input));
+    private <T> T thenWith(final T object, final String then, final Formatter<Object, String> formatter) {
+        InvocationHandler handler = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                Object result = method.invoke(object, args);
+                scenario.then(then, formatter, result);
+                return result;
+            }
+        };
+        return (T) Fred.newProxyInstance(object.getClass(), handler);
     }
+
 }
